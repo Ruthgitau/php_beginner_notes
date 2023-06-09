42d3a5ddf27f2e935c1747d3dbb32568b1eb0e98
Ruthgitau <gitaur72@gmail.com>
2023-06-09 11:21:40 +0300

    learnt variables

--- 02_variables.php
+++ 02_variables.php
@@ -22,3 +22,18 @@
 - Variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 - Variables are case-sensitive ($name and $NAME are two different variables)
 */
+
+$name="Ruth Gitau";
+$age=19;
+$gender="Female";
+$fav_food="Chapati";
+echo "My name is $name. I am $age years old.I am a $gender and love taking $fav_food.<br>";
+
+$cars=8;
+$mum_cars=2;
+$bro_cars=2;
+$dad_cars=1;
+$remaining_cars=$cars-($mum_cars+$bro_cars+$dad_cars);
+
+echo "Joseph remained with $remaining_cars cars";
+
